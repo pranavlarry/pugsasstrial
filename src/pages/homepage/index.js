2885ead38061f5a8beed616ab2e6377f65607d75
pranavlarry <pranavlarry111@gmail.com>
2019-08-09 00:23:55 +0530

    only responsive,font and placings left

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -1,23 +1,6 @@
-// import "bootstrap/js/src/index";
-// import $ from "jquery";
-// import Accordion from "../../components/accordion/accordion.js";
-// import parallaxBanner from "../../components/parallaxbanner/parallaxbanner.js";
+import $ from "jquery";
+import "bootstrap/js/src/index";
 
-// //auto closing when clicked outside navbar or accordion
-// $('body').click(function(event){
-//     console.log(event.target);
-//     if ($('.navbar').find(event.target).length === 0){
-//       $(".navbar > .navbar-collapse").removeClass("show");
-//     }
-//     if ($('.accordion').find(event.target).length === 0){
-//       console.log("g");
-//       $(".collapse").removeClass("show");
-//       $(".btn.btn-link").addClass("collapsed");
-//     }
-//   });
 
-  // Accordion();
-
-  // parallaxBanner();
 
 
